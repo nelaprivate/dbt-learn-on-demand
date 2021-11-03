a6230057673ac64c240b205cff9b0b29272bf3e1
ionnechereja@yahoo.com <ionnechereja@yahoo.com>
2021-11-03 12:11:48 +0000

    added freshness checks for orders and payments

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -10,7 +10,7 @@ with payments as(
         --amount stored as cents, convert to dollars
         amount / 100 as amount,
         created as created_at
-    from raw.stripe.payment
+    from {{ source('stripe','payment') }}
 )
 
 select * from payments
